05f1982130bb3b78baae41256dc3c641a09b07d2
Gilaric <47851759+Gilaric@users.noreply.github.com>
2022-12-08 08:00:37 +0100

    2 files modified, 3 added, 3 deleted

--- src/app/View/topbar/topbar.component.ts
+++ src/app/View/topbar/topbar.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.css']
+  styleUrls: ['./topbar.component.css'],
 })
 export class TopbarComponent implements OnInit {
+  imageSrc = 'assets/images/iphone.png';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
